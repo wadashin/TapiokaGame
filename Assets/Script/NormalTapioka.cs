bb9ccf16cc09733498c982f8e662af69c840d734
wadashin <Shin.86.miso@outlook.com>
2022-09-02 00:23:25 +0900

    タピオカを吸うようにした

--- Assets/Script/NormalTapioka.cs
+++ Assets/Script/NormalTapioka.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class NormalTapioka : TapiokaBase
 {
+    Rigidbody2D _rb;
+
     [SerializeField] float _speed = 2;
 
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         if (strawpoint)
         {
-            transform.position = Vector2.MoveTowards(transform.position, strawpoint.transform.position, _speed);
+            //transform.position = Vector2.MoveTowards(transform.position, strawpoint.transform.position, _speed);
+            _rb.velocity = Vector2.up;
+            transform.position = new Vector2(strawpoint.transform.position.x, transform.position.y);
         }
     }
 
     public override void Absorption(Transform straw)
     {
         strawpoint = straw;
-        Destroy(GetComponent<Rigidbody2D>());
+        _rb.gravityScale = 0;
     }
 }
